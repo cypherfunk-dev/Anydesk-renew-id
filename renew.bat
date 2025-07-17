21af238eb35f16880650418a5e95e37e5e0951dd
Carlos González <119871541+cypherfunk-dev@users.noreply.github.com>
2025-07-17 09:51:00 -0400

    Update renew.bat

--- renew.bat
+++ renew.bat
@@ -5,7 +5,7 @@ if %errorlevel% NEQ 0 (
 	powershell  -Command "Start-Process '%~f0' -Verb RunAs"
 	exit /b
 	)
-
+taskkill /IM AnyDesk.exe /F
 cd %programdata%\Anydesk
 del service.conf
 if errorlevel 1 (
