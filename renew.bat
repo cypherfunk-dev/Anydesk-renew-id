d7fbfc7c5628757d84ae3d5a6ebaf86c7a934bb3
Carlos González <119871541+cypherfunk-dev@users.noreply.github.com>
2025-07-17 09:13:01 -0400

    Add files via upload

---
+++ renew.bat
@@ -0,0 +1,16 @@
+@echo off
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+	echo Solicitando permisos de Admninistrador...
+	powershell  -Command "Start-Process '%~f0' -Verb RunAs"
+	exit /b
+	)
+
+cd %programdata%\Anydesk
+del service.conf
+if errorlevel 1 (
+	echo [Error] Algo salió mal...
+	goto :EOF
+)
+cd "%ProgramFiles(x86)%\AnyDesk"
+AnyDesk.exe
